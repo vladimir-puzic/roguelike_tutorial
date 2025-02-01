0d79c106024308292ca1b33f4ebabd10cc1a1105
Vladimir Puzic <vladimir.puzic@gmail.com>
2025-02-01 16:12:52 +0100

    part 2: game engine

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python3
 import tcod
 
-from actions import EscapeAction, MovementAction
+from engine import Engine
+from entity import Entity
 from input_handlers import EventHandler
 
 def main() -> None:
     screen_width = 80
     screen_height = 50
-    
-    player_x = int(screen_width / 2)
-    player_y = int(screen_height / 2)
     
     tileset = tcod.tileset.load_tilesheet(
         "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
@@ -17,36 +15,26 @@ def main() -> None:
 
     event_handler = EventHandler()
 
+    player = Entity(int(screen_width / 2), int(screen_height / 2), "@", (255, 255, 255))
+    npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), "@", (255, 255, 0))
+    entities = {npc, player}
+
+    engine = Engine(entities=entities, event_handler=event_handler, player=player)
+
     with tcod.context.new_terminal(
         screen_width,
         screen_height,
         tileset=tileset,
         title="Yet Another Roguelike Tutorial",
         vsync=True,
-    ) as context:
+    ) as context:   
         root_console = tcod.Console(screen_width, screen_height, order="F")
         while True:
-            root_console.print(player_x, player_y, string="@")
+            engine.render(console=root_console, context=context)
 
-            context.present(root_console)
+            events = tcod.event.wait()
 
-            root_console.clear()
-
-            for event in tcod.event.wait():
-                if event.type == "QUIT":
-                    raise SystemExit()
-                
-                action = event_handler.dispatch(event)
-
-                if action is None:
-                    continue
-
-                if isinstance(action, MovementAction):
-                    player_x += action.dx
-                    player_y += action.dy
-
-                elif isinstance(action, EscapeAction):
-                    raise SystemExit()
+            engine.handle_events(events)
 
 
 if __name__ == "__main__":
